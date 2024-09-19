8610d677c8ac304f85b46c53e21fd48bf3cbd14f
masahiro-666 <masahirokenta666@gmail.com>
2024-09-19 19:25:49 +0700

    dd

---
+++ components/assets/js/contact_us.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDzMPR6AUSThhU1C7pzgQb85YaeZvvuYTg",
+    authDomain: "bu-open-house-2077.firebaseapp.com",
+    projectId: "bu-open-house-2077",
+    storageBucket: "bu-open-house-2077.appspot.com",
+    messagingSenderId: "811263933785",
+    appId: "1:811263933785:web:45fa817e17a164014de048",
+    measurementId: "G-TEJYNZMB9W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Handle form submission
+document.addEventListener('DOMContentLoaded', () => {
+  const contactForm = document.getElementById('contactForm');
+
+  contactForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    // Get form values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    try {
+      // Save form data to Firestore
+      await addDoc(collection(db, 'contacts'), {
+        name: name,
+        email: email,
+        message: message,
+        timestamp: serverTimestamp()
+      });
+      console.log('send');
+      contactForm.reset();
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+  });
+});
